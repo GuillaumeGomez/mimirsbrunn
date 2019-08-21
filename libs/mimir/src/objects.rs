851a1f0b1a3c3bfec6e7e4b1da2922363fa3ae72
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-08-21 11:26:07 +0200

    Add administrative regions for admins as well

--- libs/mimir/src/objects.rs
+++ libs/mimir/src/objects.rs
@@ -529,6 +529,7 @@ pub struct Admin {
         default
     )]
     pub boundary: Option<geo::MultiPolygon<f64>>,
+    pub administrative_regions: Vec<Arc<Admin>>,
 
     #[serde(
         serialize_with = "serialize_rect",
